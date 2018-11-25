dde02a560109d1cba79babb302c8c9953da703a4
JoseVf@AsusVivoBook <jose9909@gmail.com>
2018-11-25 08:49:59 -0500

    Textos de 'Acerca de' en el archivo de textos

--- src/commons/texts.js
+++ src/commons/texts.js
@@ -22,14 +22,23 @@ let msg_questions_es = {
   str_title_default: ""
 }
 
+let msg_about_es = {
+  str_mision_body: "Kadmiel es un estudio de desarrollo con división de mantenimiento de software y hardware la cual se dedica principalmente a crear software personalizado con énfasis en la elaboración de video juegos que dejen por lo menos una enseñanza para la vida con cada juego desarrollado.",
+  str_mision_title: "Nuestra Misión:",
+  str_vision_title: "Nuestra Visión:",
+  str_vision_body: "Kadmiel S.A.S. proyecta para el 2020 ser reconocida por su altísima calidad en desarrollo de software y mantenimiento de computadores y dispositivos móviles a nivel nacional como empresa referencia de vanguardia, innovación y honestidad tanto por el ámbito empresarial como por el de nuestros usuarios y siempre con una ética intachable.",
+}
+
 var msg_login = msg_login_es;
 var msg_home = msg_home_es;
 var msg_themes = msg_themes_es;
 var msg_questions = msg_questions_es;
+var msg_about = msg_about_es;
 
 module.exports = {
   msg_login
   ,msg_home
   ,msg_themes
   ,msg_questions
+  ,msg_about
 };
--- src/components/aboutUs.js
+++ src/components/aboutUs.js
@@ -8,6 +8,8 @@ import {
 
 import Template from '../commons/template';
 
+import {msg_about} from '../commons/texts';
+
 class AboutUs extends Template {
 
   static navigationOptions = {
@@ -21,10 +23,10 @@ class AboutUs extends Template {
   render(){
     return(
       <ScrollView style={{flex: 1, paddingTop: 40, paddingHorizontal: 5}}>
-        <Text style={{fontSize: 24}}>{'Nuestra Misión:'}</Text>
-        <Text>{'Kadmiel es un estudio de desarrollo con división de mantenimiento de software y hardware la cual se dedica principalmente a crear software personalizado con énfasis en la elaboración de video juegos que dejen por lo menos una enseñanza para la vida con cada juego desarrollado.'}</Text>
-        <Text style={{fontSize: 24, marginTop:20}}>{'Nuestra Visión:'}</Text>
-        <Text>{'Kadmiel S.A.S. proyecta para el 2020 ser reconocida por su altísima calidad en desarrollo de software y mantenimiento de computadores y dispositivos móviles a nivel nacional como empresa referencia de vanguardia, innovación y honestidad tanto por el ámbito empresarial como por el de nuestros usuarios y siempre con una ética intachable.'}</Text>
+        <Text style={{fontSize: 24}}>{msg_about.str_mision_body}</Text>
+        <Text>{msg_about.str_mision_title}</Text>
+        <Text style={{fontSize: 24, marginTop:20}}>{msg_about.str_vision_title}</Text>
+        <Text>{msg_about.str_vision_body}</Text>
         <Text style={{marginTop:20}}>{'Servicio técnico en dispositivos con S.O. tanto en software como en hardware además de la creación de juegos, software, paginas web, intranet y más.'}</Text>
       </ScrollView>
     )
