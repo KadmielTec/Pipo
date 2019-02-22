ebfa4e3f090efaec4627c8b12ca74d5d1d263090
JoseVf@MacMini1 <sistemas7@monitoreointeligente.com>
2019-02-22 08:47:34 -0500

    Retoques en el login

--- src/components/login/loginView.js
+++ src/components/login/loginView.js
@@ -5,14 +5,24 @@ import {
   Text
   ,View
   ,Image
+  ,TouchableWithoutFeedback
 } from 'react-native';
-
+import {
+  Container,
+  Header,
+  Body,
+  Footer,
+  Title,
+  Content,
+  Item,
+  Input,
+  Label,
+  Form,
+  Button
+} from "native-base";
 import { msg_login } from '../../commons/texts';
-import { FormLabel, FormInput, Card } from 'react-native-elements'
-import { Container, Header, Body, Footer, Title, Content, Item, Input, Label, Form, Button } from "native-base";
 import LoginController from './loginController';
 import LoginModel from './loginModel';
-// import Button from '../../commons/components/button';
 import Template from '../../commons/template';
 import Visible from '../../commons/components/visible';
 import Icon from '../../commons/components/icon';
@@ -34,8 +44,8 @@ class LoginView extends Template {
     return(
       <Container>
         <Header androidStatusBarColor={'#FF7B08'} style={{ height: 150, backgroundColor: 'white' }}>
-					<Body style={{ alignItems: "center" }}>
-            <View>
+					<Body style={{ alignItems: "center", justifyContent: 'center' }}>
+            <View style={{ alignItems: "center", justifyContent: 'center' }}>
               <Icon name={"pipo"} size={104} color={'#FF7B08'} />
               <View padder>
                 <Text style={{color: '#5A5A5A', fontFamily: 'Ubuntu', fontStyle: 'italic'}}>Aprende con Pipo</Text>
@@ -45,106 +55,115 @@ class LoginView extends Template {
 				</Header>
 
         <Content style={{backgroundColor: '#EBEBEB'}}>
-            {login &&
-              <View>
-                <Form>
-                  <Item floatingLabel>
-                    <Label>{"Usuario"}</Label>
-                    <Input
-                      onChangeText={(text)=>{this.setState({username: text})}}
-                      blurOnSubmit={false}
-                      selectTextOnFocus={true}
-                      autoCorrect={false}
-                      autoCapitalize='none'
-                    />
-                  </Item>
+          {login &&
+            <View>
+              <Form>
+                <Item floatingLabel>
+                  <Label>{"Usuario"}</Label>
+                  <Input
+                    onChangeText={(text)=>{this.setState({username: text})}}
+                    blurOnSubmit={false}
+                    selectTextOnFocus={true}
+                    autoCorrect={false}
+                    autoCapitalize='none'
+                  />
+                </Item>
 
-                  <Item floatingLabel>
-                    <Label>{"Contraseña"}</Label>
-                    <Input
-                      onChangeText={(text)=>{this.setState({password: text})}}
-                      onSubmitEditing={this.controller.login}
-                      blurOnSubmit={false}
-                      selectTextOnFocus={true}
-                      autoCorrect={false}
-                      autoCapitalize={'none'}
-                      secureTextEntry={true}
-                    />
-                  </Item>
-                </Form>
+                <Item floatingLabel>
+                  <Label>{"Contraseña"}</Label>
+                  <Input
+                    onChangeText={(text)=>{this.setState({password: text})}}
+                    onSubmitEditing={this.controller.login}
+                    blurOnSubmit={false}
+                    selectTextOnFocus={true}
+                    autoCorrect={false}
+                    autoCapitalize={'none'}
+                    secureTextEntry={true}
+                  />
+                </Item>
+              </Form>
 
-                <View padder>
-                  <Button
-                    block
-                    light
-                    title={msg_login.login}
-                    onPress={this.controller.login}
-                  >
-                    <Text>Login</Text>
-                  </Button>
-                </View>
-
-                <Text onPress={() => this.controller.changeForm()}>Register</Text>
+              <View style={{padding: 10}}>
+                <Button
+                  block
+                  light
+                  title={msg_login.login}
+                  onPress={this.controller.login}
+                >
+                  <Text>Iniciar sesión</Text>
+                </Button>
               </View>
-            }
-            {!login &&
-              <View>
-                <Form>
-                  <Item floatingLabel>
-                    <Label>{"Usuario"}</Label>
-                    <Input
-                      onChangeText={(text)=>{this.setState({username: text})}}
-                      blurOnSubmit={false}
-                      selectTextOnFocus={true}
-                      autoCorrect={false}
-                      autoCapitalize='none'
-                    />
-                  </Item>
 
-                  <Item floatingLabel>
-                    <Label>{"Contraseña"}</Label>
-                    <Input
-                      onChangeText={(text)=>{this.setState({password: text})}}
-                      blurOnSubmit={false}
-                      selectTextOnFocus={true}
-                      autoCorrect={false}
-                      autoCapitalize={'none'}
-                      secureTextEntry={true}
-                    />
-                  </Item>
-
-                  <Item floatingLabel >
-                    <Label>{"Confirmar contraseña"}</Label>
-                    <Input
-                      onChangeText={(text)=>{this.setState({password2: text})}}
-                      onSubmitEditing={this.controller.register}
-                      blurOnSubmit={false}
-                      selectTextOnFocus={true}
-                      autoCorrect={false}
-                      autoCapitalize={'none'}
-                      secureTextEntry={true}
-                    />
-                  </Item>
-                </Form>
-
-                <View padder>
-                  <Button
-                    block
-                    light
-                    title={msg_login.register}
-                    onPress={this.controller.register}
-                  >
-                    <Text>Register</Text>
-                  </Button>
+              <TouchableWithoutFeedback onPress={() => this.controller.changeForm()} style={{padding: 10}}>
+                <View style={{ alignItems: 'center'}}>
+                  <Text>Registrarse</Text>
                 </View>
+              </TouchableWithoutFeedback>
+            </View>
+          }
+          {!login &&
+            <View>
+              <Form>
+                <Item floatingLabel>
+                  <Label>{"Usuario"}</Label>
+                  <Input
+                    onChangeText={(text)=>{this.setState({username: text})}}
+                    blurOnSubmit={false}
+                    selectTextOnFocus={true}
+                    autoCorrect={false}
+                    autoCapitalize='none'
+                  />
+                </Item>
+
+                <Item floatingLabel>
+                  <Label>{"Contraseña"}</Label>
+                  <Input
+                    onChangeText={(text)=>{this.setState({password: text})}}
+                    blurOnSubmit={false}
+                    selectTextOnFocus={true}
+                    autoCorrect={false}
+                    autoCapitalize={'none'}
+                    secureTextEntry={true}
+                  />
+                </Item>
+
+                <Item floatingLabel >
+                  <Label>{"Confirmar contraseña"}</Label>
+                  <Input
+                    onChangeText={(text)=>{this.setState({password2: text})}}
+                    onSubmitEditing={this.controller.register}
+                    blurOnSubmit={false}
+                    selectTextOnFocus={true}
+                    autoCorrect={false}
+                    autoCapitalize={'none'}
+                    secureTextEntry={true}
+                  />
+                </Item>
+              </Form>
+
+              <View style={{padding: 10}}>
+                <Button
+                  block
+                  light
+                  title={msg_login.register}
+                  onPress={this.controller.register}
+                >
+                  <Text>Registrar</Text>
+                </Button>
               </View>
-            }
+              <TouchableWithoutFeedback onPress={() => this.controller.changeForm()} style={{padding: 10}}>
+                <View style={{alignItems: 'center'}}>
+                  <Text>Volver</Text>
+                </View>
+              </TouchableWithoutFeedback>
+            </View>
+          }
         </Content>
 
         <Footer style={{ backgroundColor: "white" }}>
 					<View style={{ alignItems: "center", opacity: 0.5, flexDirection: "row" }}>
 						<View padder>
-							<Text style={{ color: "#000", fontFamily: 'Ubuntu' }}>Made with love at Kadmiel</Text>
+							<Text style={{ color: "#000", fontFamily: 'Ubuntu' }}>Hecho con amor en Kadmiel</Text>
 						</View>
 					</View>
 				</Footer>
