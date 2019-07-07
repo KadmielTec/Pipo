f116ea8851af816e3c808f8b308517747a3a9911
Jose Vergara <josevergara@seven4n.com>
2019-07-07 11:55:32 -0500

    Se corrigen errores ortográficos

--- src/components/aboutUs.js
+++ src/components/aboutUs.js
@@ -20,7 +20,7 @@ class AboutUs extends Template {
     return(
       <ScrollView style={{flex: 1, paddingTop: 40, paddingHorizontal: 5}}>
         <Text>
-          {'Pipo es un Pingüino al que le encanta aprender y enseñar lo que sabe sobre muchas cosas de diversos temas que a través de los años a ido aprendiendo, pero sólo hasta ahora y luego de mucho tiempo de estudio a decidido enseñar o reforzar los temas de las diferentes categorías de preguntas que le han interesado. Pipo es un Pingüino al que también le gusta que le enseñen y está dispuesto a aprender incluso sobre otros temas que puede que a "ti" te interesen.'}
+          {'Pipo es un pingüino al que le encanta aprender y enseñar lo que sabe sobre muchas cosas de diversos temas que a través de los años ha ido aprendiendo, pero sólo hasta ahora y luego de mucho tiempo de estudio a decidido enseñar o reforzar los temas de las diferentes categorías de preguntas que le han interesado. Pipo es un pingüino al que también le gusta que le enseñen y está dispuesto a aprender incluso sobre otros temas que puede que a ti te interesen.'}
         </Text>
       </ScrollView>
     )
